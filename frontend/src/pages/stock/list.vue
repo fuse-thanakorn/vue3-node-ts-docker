d82bcc89085b08c9b149bc92587108f71c8b78ff
slowhand-13 <thanakorn@idio-tech.com>
2023-01-13 18:32:20 +0700

    :rocket: tailwind

--- frontend/src/pages/stock/list.vue
+++ frontend/src/pages/stock/list.vue
@@ -19,9 +19,32 @@ watchEffect(() => {
     <h2 v-if="stocks.stocks">
       Here The Result  :
     </h2>
-    <span>
-      {{ stocks.stocks }}
-    </span>
+    <table class="table-auto">
+      <thead>
+        <tr>
+          <th>Song</th>
+          <th>Artist</th>
+          <th>Year</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+          <td>Malcolm Lockyer</td>
+          <td>1961</td>
+        </tr>
+        <tr>
+          <td>Witchy Woman</td>
+          <td>The Eagles</td>
+          <td>1972</td>
+        </tr>
+        <tr>
+          <td>Shining Star</td>
+          <td>Earth, Wind, and Fire</td>
+          <td>1975</td>
+        </tr>
+      </tbody>
+    </table>
     <button
       btn m="3 t6" text-md
       @click="onStocksClick"
